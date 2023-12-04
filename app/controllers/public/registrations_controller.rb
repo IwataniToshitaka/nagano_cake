f832a4a4d35fe27c6e2a78a72e9fbbcff7176535
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-12-04 21:21:15 +0000

    [sign_path]nagano_cake

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -41,14 +41,15 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+   def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
+  #このコメントアウトを外してkeysをnameにすればサインイン時のストロングパラメータとして使える
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
@@ -59,4 +60,16 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def after_sign_in_path_for(resource)
+    #遷移先のパス
+  end
+  　#ユーザ登録した後の遷移先指定
+
+
+  def after_sing_up_path_for(resource)
+    #遷移先のパス
+  end
+  　#ユーザがサイトにログインした後の遷移先指定
+
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,20 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(resource)
+    #遷移先のパス
+  end
+    #既に登録済みのユーザがログインした後の遷移先指定
+
+  def after_sing_up_path_for(resource)
+    #遷移先のパス
+  end
+    #ユーザがサイトにログインした後の遷移先指定
+
+  def after_sign_out_path_for(resource)
+    #遷移先のパス
+  end
+    #ユーザがサインアウトした後の遷移先指定
+
 end
