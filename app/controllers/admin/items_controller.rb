86e73a5e6db12ec561436c971c8736dae55b828a
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-12-10 21:37:58 +0000

    [Continue-newitems]nagano_cake

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,3 +1,26 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
+
+  def new
+  end
+
+  #商品の詳細画面表示
+  def show
+  end
+
+  # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.save
+    redirect_to admin_new_items_path
+  end
+
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:name, :image, :introduction, :price)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ devise_for :customers, skip: [:passwords], controllers: {
     #顧客情報編集画面への遷移
   get 'customer/orders', to: 'customers#index'
 
-  patch 'customer/withdraw', to: 'public/customers#/withdraw'
+  patch 'customer/withdraw', to: 'customers#withdraw'
     #顧客の退会ステータス
   end
 
@@ -27,6 +27,9 @@ devise_for :customers, skip: [:passwords], controllers: {
   }
   namespace :admin do
   get 'top', to: 'homes#top'
+  get 'items/new', to: 'items#new'
+  get 'items/:id', to: 'items#show' #商品詳細画面遷移
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
