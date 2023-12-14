6f897c657cccaa7c36df0229e3a1973e4aeb86be
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-12-14 02:07:50 +0000

    [Continue]admintop

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get '/', to: 'public/homes#top'
+  get 'about', to: 'public/homes#about'
+
   #顧客用
   #URL /customers/sign_in ...
 devise_for :customers, skip: [:passwords], controllers: {
@@ -7,6 +10,8 @@ devise_for :customers, skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
   scope module: :public do
+
+
   get 'customer/my_page', to: 'customers#show'
     #顧客新規登録後のマイページへの遷移
   get 'customer/sign_in', to: 'customers#new'
@@ -28,7 +33,7 @@ devise_for :customers, skip: [:passwords], controllers: {
   namespace :admin do
   get 'top', to: 'homes#top'
   resources :items, only: [:new, :create, :show, :index]
-    #商品詳細画面遷移
+    #商品詳 細画面遷移
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
