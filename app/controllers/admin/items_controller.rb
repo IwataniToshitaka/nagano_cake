b55635dacfa2bd8e207ded7208540ef2b7e6fe42
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-12-16 20:33:37 +0000

    [done]itemspatch

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -26,16 +26,29 @@ class Admin::ItemsController < ApplicationController
     end
   end
 
-
-
   def index
     @items = Item.page(params[:page])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
+  else
+    render :edit
+  end
+  end
+
+
 
   # 投稿データのストロングパラメータ
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :image)
+    params.require(:item).permit(:name, :introduction, :price, :image, :edit)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ devise_for :customers, skip: [:passwords], controllers: {
   }
   namespace :admin do
   get 'top', to: 'homes#top'
-  resources :items, only: [:new, :create, :show, :index]
+  resources :items, only: [:new, :create, :show, :index, :edit, :update]
     #商品詳細画面遷移
   end
 
